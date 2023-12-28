cbb50944033b8c2804a0c682a1e51f7849a213ef
VickyAruldoss <jamvicky3094@gmail.com>
2023-12-29 03:04:42 +0530

    TECH | Added  Lint and prettier and updated the CI config too

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
 const App = (): JSX.Element => {
   return (
-    <div className="App">
-      <p>Successfully Deployed <code>A React application</code> to Netlify with CircleCI version 3</p>
+    <div className='App'>
+      <p>
+        Successfully Deployed <code>A React application</code> to Netlify with CircleCI version 3
+      </p>
     </div>
   )
 }
