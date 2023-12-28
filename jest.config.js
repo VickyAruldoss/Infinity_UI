12896734ccf51b9f8377db89d03d2124dc5dea16
VickyAruldoss <jamvicky3094@gmail.com>
2023-12-28 21:52:35 +0530

    Added jest testing lib

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "ts-jest",
+  },
+  testEnvironment: 'jsdom',
+};
